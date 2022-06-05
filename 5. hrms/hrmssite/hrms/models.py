d6ec06544f71b433d01909bc2e72a23f2153111c
Noman <noman.islam@hotmail.com>
2022-06-05 20:49:00 +0500

    django flask

--- 5. hrms/hrmssite/hrms/models.py
+++ 5. hrms/hrmssite/hrms/models.py
@@ -4,8 +4,18 @@ from django.db import models
 from django.db import models
 
 class Attendance(models.Model):
+    @property
+    def hours(self):
+        if(self.time_out):
+            diff = self.time_out-self.time_in
+            return diff.total_seconds() / 3600
+        else:
+            return 0
+
+
     emp_id = models.CharField(max_length=20)
     date = models.DateTimeField(max_length=20)
     time_in = models.DateTimeField(max_length=20)
     time_out = models.DateTimeField(max_length=20, null=True)
+
 
--- 5. hrms/hrmssite/hrms/views.py
+++ 5. hrms/hrmssite/hrms/views.py
@@ -1,8 +1,9 @@
 from email import message
 from django.shortcuts import render
 from django.http import HttpResponse
-from datetime import datetime
+from datetime import datetime, date, timedelta
 from .models import Attendance
+from django.db.models import Sum
 
 # Create your views here.
 
@@ -27,7 +28,36 @@ def index(request):
 			message = 'You have successfully timed in'
 
 		#getting all the attendance
-		attds = Attendance.objects.filter(emp_id=emp_id)
-		return render(request, 'attendance.html', {'message':message, 'attendance': attds})
+		start, end = get_current_week_range()
+		attds = Attendance.objects.filter(emp_id=emp_id, date__gte=start, date__lte=end)
+		total = 0
+		for a in attds:
+			total += a.hours
+		return render(request, 'attendance.html', {'message':message, 'attendance': attds, 'total': total})
 	else:
 		return render(request, 'attendance.html', {'message': ''})
+
+
+def weekly(request):
+	try:
+		start, end = get_current_week_range()
+		a = Attendance.objects.filter(date__gte=start, date__lte=end)
+		return render(request, 'weekly.html', {'message':message, 'attendance': a})
+	except Attendance.DoesNotExist:
+		return HttpResponse("No attendance for current week")
+
+def daily(request):
+	try:		
+		today = datetime.now()
+		dt = today.strftime("%Y-%m-%d")
+		dt = dt + " 00:00"
+		a = Attendance.objects.filter(date=dt)
+		return render(request, 'daily.html', {'message':message, 'attendance': a})
+	except Attendance.DoesNotExist:
+		return HttpResponse("No attendance for today")
+
+def get_current_week_range():
+	today = date.today()
+	start = today - timedelta(days=today.weekday())
+	end = start + timedelta(days=6)
+	return (start,end)
